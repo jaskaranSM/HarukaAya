7a92d6fb26c999fbb94bd4d6eb48da22e08180a0
D Avinash Kumar <56125142+davinash97@users.noreply.github.com>
2020-02-15 12:47:20 +0530

    Oopz😂

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -18,6 +18,8 @@ LOGGER.info("Starting haruka...")
 #    LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
 #    quit(1)
     
+ENV = bool(os.environ.get('ENV', False))
+
 if ENV:
     TOKEN = os.environ.get('TOKEN', None)
     try:
